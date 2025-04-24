00caf2a9011427844cf740ed3acead2e72425892
sanmart22 <francosanmartino22@gmail.com>
2025-04-24 03:28:20 -0300

    interfaz de usuario

---
+++ main.py
@@ -0,0 +1,40 @@
+from usuarios.gestion_usuarios import registrar_usuario, iniciar_sesion
+from juego.blackjack import jugar_blackjack
+from juego.historial import ver_historial
+
+def mostrar_menu():
+    print("\nBienvenido al Casino Python!")
+    print("1. Registrarse")
+    print("2. Iniciar Sesión")
+    print("3. Jugar Blackjack")
+    print("4. Ver Historial")
+    print("5. Salir")
+
+def main():
+    usuario_actual = None
+    while True:
+        mostrar_menu()
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == '1':
+            registrar_usuario()
+        elif opcion == '2':
+            usuario_actual = iniciar_sesion()
+        elif opcion == '3':
+            if usuario_actual:
+                jugar_blackjack(usuario_actual)
+            else:
+                print("Debes iniciar sesión para jugar.")
+        elif opcion == '4':
+            if usuario_actual:
+                ver_historial(usuario_actual)
+            else:
+                print("Debes iniciar sesión para ver el historial.")
+        elif opcion == '5':
+            print("Gracias por visitar el Casino Python!")
+            break
+        else:
+            print("Opción inválida.")
+
+if __name__ == "__main__":
+    main()
