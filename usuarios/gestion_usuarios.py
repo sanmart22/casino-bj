0e7d5ef9a78e006a57d32c8939f8972e8c3046bb
sanmart22 <francosanmartino22@gmail.com>
2025-05-29 10:51:22 -0300

    ADD except

--- usuarios/gestion_usuarios.py
+++ usuarios/gestion_usuarios.py
@@ -1,24 +1,37 @@
+class UsuarioExistenteError(Exception):
+    pass
+class UsuarioNoEncontradoError(Exception):
+    pass
+class ContrasenaIncorrectaError(Exception):
+    pass
 usuarios = {}
 
 def registrar_usuario():
-    nombre = input("Ingrese nombre de usuario: ").strip()
-    if nombre.lower() in (n.lower() for n in usuarios):
-        print("Este nombre de usuario ya existe")
-        return
-    contrasena = input("Ingrese contraseña: ").strip()
-    usuarios[nombre] = contrasena
-    print(f"Usuario '{nombre.upper()}' registrado con exito")
+    try:
+        nombre = input("Ingrese nombre de usuario: ").strip()
+        if nombre.lower() in (n.lower() for n in usuarios):
+            raise UsuarioExistenteError(f"El usuario '{nombre}' ya existe")
+        
+        contrasena = input("Ingrese contraseña: ").strip()
+        usuarios[nombre] = contrasena
+        print(f"Usuario '{nombre.upper()}' registrado con exito")
+
+    except UsuarioExistenteError as e:
+        print(f"Error: {e}")
 
 def iniciar_sesion():
-    nombre = input("Ingrese nombre de usuario: ").strip()
-    if nombre not in usuarios:
-        print("Usuario no encontrado")
-        return None
-    contrasena = input("Ingrese contraseña: ").strip()
-    if usuarios[nombre] == contrasena:
-        print(f"Bienvenido {nombre.capitalize()}!")
+    try:
+        nombre = input("Ingrese nombre de usuario: ").strip()
+        if nombre not in usuarios:
+            raise UsuarioNoEncontradoError(f"El usuario '{nombre}' no existe")
+        
+        contrasena = input("Ingrese contraseña: ").strip()
+        if usuarios[nombre] != contrasena:
+            raise ContrasenaIncorrectaError("Contraseña incorrecta")
+        else:
+            print(f"Bienvenido, {nombre.upper()}!")
         return nombre
-    else:
-        print("Contraseña incorrecta")
-        return None
-    
+    except UsuarioNoEncontradoError as e:
+        print(f"Error: {e}")
+    except ContrasenaIncorrectaError as e:
+        print(f"Error: {e}")
